77a280631be855b2fa679975835653afe9c7b6f1
BugHunter-X <askzenai@gmail.com>
2024-12-26 14:30:06 +0100

    Add FixedLoop.java file

---
+++ FixedLoop.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    public static void main(String[] args) {
+        for (int x = 0; x < 10; x++) {
+            System.out.println(x);
+            if (x == 5) {
+                //Example of what could be done
+                //break; 
+            }
+        }
+        System.out.println("Loop finished");
+    }
+}
